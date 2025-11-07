51ec2b40b22a91170086a21c16a80bc001f59fa4
sahwho <vikash.sahu@mcpsmd.net>
2025-11-07 07:46:48 -0500

    Update Fall2025Part3PublicTests.java

--- Fall2025Part3PublicTests.java
+++ Fall2025Part3PublicTests.java
@@ -1,3 +1,4 @@
+
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
@@ -292,5 +293,314 @@ public class Fall2025Part3PublicTests {
 		ArrayList<Piece> pieces = b.play(p);
 
 		assertTrue(pieces.isEmpty());
+	}
+	
+	@Test
+	public void testGameWithSorting1() {
+		Board b = new Board();
+		Piece p = new Piece("c4", Piece.Color.BLACK);
+		assertEquals(Piece.Color.BLACK, b.getCurrentPlayer());
+		b.play(p);
+
+		/* verify state of board after playing c4B */
+		assertEquals(59, b.countColorPieces(Piece.Color.EMPTY));
+		assertEquals(4, b.countColorPieces(Piece.Color.BLACK));
+		assertEquals(1, b.countColorPieces(Piece.Color.WHITE));
+		assertEquals(p.toString(), b.getBoard()[3][2].toString());
+		assertEquals(Piece.Color.BLACK, b.getBoard()[3][3].getColor());
+		assertEquals(Piece.Color.BLACK, b.getBoard()[3][4].getColor());
+		assertEquals(Piece.Color.BLACK, b.getBoard()[4][3].getColor());
+		assertEquals(Piece.Color.WHITE, b.getBoard()[4][4].getColor());
+		/* end verification of state of board after playing c4B*/
+
+		assertEquals(Piece.Color.WHITE, b.getCurrentPlayer());
+		Piece p2 = new Piece("e3", Piece.Color.WHITE);
+		b.play(p2);
+
+		/* verify state of board after playing e3W */
+		assertEquals(58, b.countColorPieces(Piece.Color.EMPTY));
+		assertEquals(3, b.countColorPieces(Piece.Color.BLACK));
+		assertEquals(3, b.countColorPieces(Piece.Color.WHITE));
+		assertEquals(p2.toString(), b.getBoard()[2][4].toString());
+		assertEquals(Piece.Color.BLACK, b.getBoard()[3][2].getColor());
+		assertEquals(Piece.Color.BLACK, b.getBoard()[3][3].getColor());
+		assertEquals(Piece.Color.BLACK, b.getBoard()[4][3].getColor());
+		assertEquals(Piece.Color.WHITE, b.getBoard()[2][4].getColor());
+		assertEquals(Piece.Color.WHITE, b.getBoard()[3][4].getColor());
+		assertEquals(Piece.Color.WHITE, b.getBoard()[4][4].getColor());
+		/* end verification of state of board after playing e3W*/
+
+		Piece p3 = new Piece("f6", Piece.Color.BLACK);
+		ArrayList<Piece> piecesToCapture = b.checkDiagonalUpLeft(p3);
+
+		assertEquals(1, piecesToCapture.size());
+		//check that other direction checks are empty
+		assertEquals(0, b.checkHorizontalRight(p3).size());
+		assertEquals(0, b.checkVerticalDown(p3).size());
+		assertEquals(new Piece("e5", Piece.Color.WHITE), piecesToCapture.get(0));
+
+		assertEquals(Piece.Color.BLACK, b.getCurrentPlayer());
+		b.play(p3);
+
+		/* verify state of board after playing f6B */
+		assertEquals(57, b.countColorPieces(Piece.Color.EMPTY));
+		assertEquals(5, b.countColorPieces(Piece.Color.BLACK));
+		assertEquals(2, b.countColorPieces(Piece.Color.WHITE));
+		assertEquals(p3.toString(), b.getBoard()[5][5].toString());
+		assertEquals(Piece.Color.BLACK, b.getBoard()[3][2].getColor());
+		assertEquals(Piece.Color.BLACK, b.getBoard()[3][3].getColor());
+		assertEquals(Piece.Color.BLACK, b.getBoard()[4][3].getColor());
+		assertEquals(Piece.Color.BLACK, b.getBoard()[4][4].getColor());
+		assertEquals(Piece.Color.BLACK, b.getBoard()[5][5].getColor());
+		assertEquals(Piece.Color.WHITE, b.getBoard()[2][4].getColor());
+		assertEquals(Piece.Color.WHITE, b.getBoard()[3][4].getColor());
+		/* end verification of state of board after playing f6B*/
+
+		//should capture 2 black pieces (horizontal right only)
+		Piece p4 = new Piece("b4", Piece.Color.WHITE);
+		piecesToCapture = b.checkHorizontalRight(p4);
+		//check that other direction checks are empty
+		assertEquals(0, b.checkDiagonalUpLeft(p4).size());
+		assertEquals(0, b.checkVerticalDown(p4).size());
+		assertEquals(2, piecesToCapture.size());
+		assertEquals("c4B", piecesToCapture.get(0).toString());
+		assertEquals("d4B", piecesToCapture.get(1).toString());
+
+		assertEquals(Piece.Color.WHITE, b.getCurrentPlayer());
+	}
+	
+	@Test
+	public void testGameWithSorting2() {
+		Board b = new Board();
+		Piece p = new Piece("c4", Piece.Color.BLACK);
+		assertEquals(Piece.Color.BLACK, b.getCurrentPlayer());
+		b.play(p);
+
+		/* verify state of board after playing c4B */
+		assertEquals(59, b.countColorPieces(Piece.Color.EMPTY));
+		assertEquals(4, b.countColorPieces(Piece.Color.BLACK));
+		assertEquals(1, b.countColorPieces(Piece.Color.WHITE));
+		assertEquals(p.toString(), b.getBoard()[3][2].toString());
+		assertEquals(Piece.Color.BLACK, b.getBoard()[3][3].getColor());
+		assertEquals(Piece.Color.BLACK, b.getBoard()[3][4].getColor());
+		assertEquals(Piece.Color.BLACK, b.getBoard()[4][3].getColor());
+		assertEquals(Piece.Color.WHITE, b.getBoard()[4][4].getColor());
+		/* end verification of state of board after playing c4B*/
+
+		assertEquals(Piece.Color.WHITE, b.getCurrentPlayer());
+		Piece p2 = new Piece("e3", Piece.Color.WHITE);
+		b.play(p2);
+
+		/* verify state of board after playing e3W */
+		assertEquals(58, b.countColorPieces(Piece.Color.EMPTY));
+		assertEquals(3, b.countColorPieces(Piece.Color.BLACK));
+		assertEquals(3, b.countColorPieces(Piece.Color.WHITE));
+		assertEquals(p2.toString(), b.getBoard()[2][4].toString());
+		assertEquals(Piece.Color.BLACK, b.getBoard()[3][2].getColor());
+		assertEquals(Piece.Color.BLACK, b.getBoard()[3][3].getColor());
+		assertEquals(Piece.Color.BLACK, b.getBoard()[4][3].getColor());
+		assertEquals(Piece.Color.WHITE, b.getBoard()[2][4].getColor());
+		assertEquals(Piece.Color.WHITE, b.getBoard()[3][4].getColor());
+		assertEquals(Piece.Color.WHITE, b.getBoard()[4][4].getColor());
+		/* end verification of state of board after playing e3W*/
+
+		Piece p3 = new Piece("f6", Piece.Color.BLACK);
+		ArrayList<Piece> piecesToCapture = b.checkDiagonalUpLeft(p3);
+
+		assertEquals(1, piecesToCapture.size());
+		//check that other direction checks are empty
+		assertEquals(0, b.checkHorizontalRight(p3).size());
+		assertEquals(0, b.checkVerticalDown(p3).size());
+		assertEquals(new Piece("e5", Piece.Color.WHITE), piecesToCapture.get(0));
+
+		assertEquals(Piece.Color.BLACK, b.getCurrentPlayer());
+		b.play(p3);
+
+		/* verify state of board after playing f6B */
+		assertEquals(57, b.countColorPieces(Piece.Color.EMPTY));
+		assertEquals(5, b.countColorPieces(Piece.Color.BLACK));
+		assertEquals(2, b.countColorPieces(Piece.Color.WHITE));
+		assertEquals(p3.toString(), b.getBoard()[5][5].toString());
+		assertEquals(Piece.Color.BLACK, b.getBoard()[3][2].getColor());
+		assertEquals(Piece.Color.BLACK, b.getBoard()[3][3].getColor());
+		assertEquals(Piece.Color.BLACK, b.getBoard()[4][3].getColor());
+		assertEquals(Piece.Color.BLACK, b.getBoard()[4][4].getColor());
+		assertEquals(Piece.Color.BLACK, b.getBoard()[5][5].getColor());
+		assertEquals(Piece.Color.WHITE, b.getBoard()[2][4].getColor());
+		assertEquals(Piece.Color.WHITE, b.getBoard()[3][4].getColor());
+		/* end verification of state of board after playing f6B*/
+
+		//should capture 2 black pieces (horizontal right only)
+		Piece p4 = new Piece("b4", Piece.Color.WHITE);
+		piecesToCapture = b.checkHorizontalRight(p4);
+		//check that other direction checks are empty
+		assertEquals(0, b.checkDiagonalUpLeft(p4).size());
+		assertEquals(0, b.checkVerticalDown(p4).size());
+		assertEquals(2, piecesToCapture.size());
+		assertEquals("c4B", piecesToCapture.get(0).toString());
+		assertEquals("d4B", piecesToCapture.get(1).toString());
+
+		assertEquals(Piece.Color.WHITE, b.getCurrentPlayer());
+
+		b.play(p4);
+
+		/* verify state of board after playing b4W */
+		assertEquals(56, b.countColorPieces(Piece.Color.EMPTY));
+		assertEquals(3, b.countColorPieces(Piece.Color.BLACK));
+		assertEquals(5, b.countColorPieces(Piece.Color.WHITE));
+		assertEquals(p4.toString(), b.getBoard()[3][1].toString());
+		assertEquals(Piece.Color.WHITE, b.getBoard()[3][1].getColor());
+		assertEquals(Piece.Color.WHITE, b.getBoard()[3][2].getColor());
+		assertEquals(Piece.Color.WHITE, b.getBoard()[3][3].getColor());
+		assertEquals(Piece.Color.BLACK, b.getBoard()[4][3].getColor());
+		assertEquals(Piece.Color.BLACK, b.getBoard()[4][4].getColor());
+		assertEquals(Piece.Color.BLACK, b.getBoard()[5][5].getColor());
+		assertEquals(Piece.Color.WHITE, b.getBoard()[2][4].getColor());
+		assertEquals(Piece.Color.WHITE, b.getBoard()[3][4].getColor());
+		/* end verification of state of board after playing b4W*/
+
+		//Should capture 2 white pieces (vertical down only)
+		Piece p5 = new Piece("e2", Piece.Color.BLACK);
+		piecesToCapture = b.checkVerticalDown(p5);
+		//check that other direction checks are empty
+		assertEquals(0, b.checkHorizontalRight(p5).size());
+		assertEquals(0, b.checkDiagonalUpLeft(p5).size());
+		assertEquals(2, piecesToCapture.size());
+		assertEquals("e3W", piecesToCapture.get(0).toString());
+		assertEquals("e4W", piecesToCapture.get(1).toString());
+
+		assertEquals(Piece.Color.BLACK, b.getCurrentPlayer());
+
+		b.play(p5);
+
+		/* verify state of board after playing e2B */
+		assertEquals(55, b.countColorPieces(Piece.Color.EMPTY));
+		assertEquals(6, b.countColorPieces(Piece.Color.BLACK));
+		assertEquals(3, b.countColorPieces(Piece.Color.WHITE));
+		assertEquals(p5.toString(), b.getBoard()[1][4].toString());
+		assertEquals(Piece.Color.BLACK, b.getBoard()[1][4].getColor());
+		assertEquals(Piece.Color.BLACK, b.getBoard()[2][4].getColor());
+		assertEquals(Piece.Color.BLACK, b.getBoard()[3][4].getColor());
+		assertEquals(Piece.Color.BLACK, b.getBoard()[4][4].getColor());
+		assertEquals(Piece.Color.BLACK, b.getBoard()[5][5].getColor());
+		assertEquals(Piece.Color.BLACK, b.getBoard()[4][3].getColor());
+
+		assertEquals(Piece.Color.WHITE, b.getBoard()[3][1].getColor());
+		assertEquals(Piece.Color.WHITE, b.getBoard()[3][2].getColor());
+		assertEquals(Piece.Color.WHITE, b.getBoard()[3][3].getColor());
+		/* end verification of state of board after playing e2B*/
+
+		Piece p6 = new Piece("g7", Piece.Color.WHITE);
+		piecesToCapture = b.checkDiagonalUpLeft(p6);
+		assertEquals(2, piecesToCapture.size());
+		//check that other direction checks are empty
+		assertEquals(0, b.checkHorizontalRight(p6).size());
+		assertEquals(0, b.checkVerticalDown(p6).size());
+		assertEquals("e5B", piecesToCapture.get(0).toString());
+		assertEquals("f6B", piecesToCapture.get(1).toString());
+
+		assertEquals(Piece.Color.WHITE, b.getCurrentPlayer());
+
+		b.play(p6);
+
+		/* verify state of board after playing g7W */
+		assertEquals(54, b.countColorPieces(Piece.Color.EMPTY));
+		assertEquals(4, b.countColorPieces(Piece.Color.BLACK));
+		assertEquals(6, b.countColorPieces(Piece.Color.WHITE));
+		assertEquals(p6.toString(), b.getBoard()[6][6].toString());
+		assertEquals(Piece.Color.WHITE, b.getBoard()[6][6].getColor());
+
+		assertEquals(Piece.Color.BLACK, b.getBoard()[1][4].getColor());
+		assertEquals(Piece.Color.BLACK, b.getBoard()[2][4].getColor());
+		assertEquals(Piece.Color.BLACK, b.getBoard()[3][4].getColor());
+		assertEquals(Piece.Color.BLACK, b.getBoard()[4][3].getColor());
+
+		assertEquals(Piece.Color.WHITE, b.getBoard()[4][4].getColor());
+		assertEquals(Piece.Color.WHITE, b.getBoard()[5][5].getColor());
+		assertEquals(Piece.Color.WHITE, b.getBoard()[3][1].getColor());
+		assertEquals(Piece.Color.WHITE, b.getBoard()[3][2].getColor());
+		assertEquals(Piece.Color.WHITE, b.getBoard()[3][3].getColor());
+		/* end verification of state of board after playing g7W*/
+
+		assertEquals(Piece.Color.BLACK, b.getCurrentPlayer());
+
+		Piece p7 = new Piece("a4", Piece.Color.BLACK);
+		piecesToCapture = b.checkHorizontalRight(p7);
+		assertEquals(3, piecesToCapture.size());
+		//check that other direction checks are empty
+		assertEquals(0, b.checkDiagonalUpLeft(p7).size());
+		assertEquals(0, b.checkVerticalDown(p7).size());
+
+		assertEquals("b4W", piecesToCapture.get(0).toString());
+		assertEquals("c4W", piecesToCapture.get(1).toString());
+		assertEquals("d4W", piecesToCapture.get(2).toString());
+
+		b.play(p7);
+
+		/* verify state of board after playing a4B */
+		assertEquals(53, b.countColorPieces(Piece.Color.EMPTY));
+		assertEquals(8, b.countColorPieces(Piece.Color.BLACK));
+		assertEquals(3, b.countColorPieces(Piece.Color.WHITE));
+		assertEquals(p7.toString(), b.getBoard()[3][0].toString());
+		assertEquals(Piece.Color.BLACK, b.getBoard()[3][0].getColor());
+
+		assertEquals(Piece.Color.WHITE, b.getBoard()[6][6].getColor());
+		assertEquals(Piece.Color.WHITE, b.getBoard()[4][4].getColor());
+		assertEquals(Piece.Color.WHITE, b.getBoard()[5][5].getColor());
+
+		assertEquals(Piece.Color.BLACK, b.getBoard()[1][4].getColor());
+		assertEquals(Piece.Color.BLACK, b.getBoard()[2][4].getColor());
+		assertEquals(Piece.Color.BLACK, b.getBoard()[3][4].getColor());
+		assertEquals(Piece.Color.BLACK, b.getBoard()[4][3].getColor());
+		assertEquals(Piece.Color.BLACK, b.getBoard()[3][1].getColor());
+		assertEquals(Piece.Color.BLACK, b.getBoard()[3][2].getColor());
+		assertEquals(Piece.Color.BLACK, b.getBoard()[3][3].getColor());
+		/* end verification of state of board after playing a4B*/
+
+		Piece p8 = new Piece("e1", Piece.Color.WHITE);
+		piecesToCapture = b.checkVerticalDown(p8);
+		assertEquals(3, piecesToCapture.size());
+		//check that other direction checks are empty
+		assertEquals(0, b.checkHorizontalRight(p8).size());
+		assertEquals(0, b.checkDiagonalUpLeft(p8).size());
+		assertEquals("e2B", piecesToCapture.get(0).toString());
+		assertEquals("e3B", piecesToCapture.get(1).toString());
+		assertEquals("e4B", piecesToCapture.get(2).toString());
+
+		assertEquals(Piece.Color.WHITE, b.getCurrentPlayer());
+
+		b.play(p8);
+		/* verify state of board after playing e1W */
+		assertEquals(52, b.countColorPieces(Piece.Color.EMPTY));
+		assertEquals(5, b.countColorPieces(Piece.Color.BLACK));
+		assertEquals(7, b.countColorPieces(Piece.Color.WHITE));
+		assertEquals(p8.toString(), b.getBoard()[0][4].toString());
+		assertEquals(Piece.Color.WHITE, b.getBoard()[0][4].getColor());
+
+		assertEquals(Piece.Color.WHITE, b.getBoard()[6][6].getColor());
+		assertEquals(Piece.Color.WHITE, b.getBoard()[4][4].getColor());
+		assertEquals(Piece.Color.WHITE, b.getBoard()[5][5].getColor());
+		assertEquals(Piece.Color.WHITE, b.getBoard()[1][4].getColor());
+		assertEquals(Piece.Color.WHITE, b.getBoard()[2][4].getColor());
+		assertEquals(Piece.Color.WHITE, b.getBoard()[3][4].getColor());
+
+		assertEquals(Piece.Color.BLACK, b.getBoard()[3][0].getColor());
+		assertEquals(Piece.Color.BLACK, b.getBoard()[4][3].getColor());
+		assertEquals(Piece.Color.BLACK, b.getBoard()[3][1].getColor());
+		assertEquals(Piece.Color.BLACK, b.getBoard()[3][2].getColor());
+		assertEquals(Piece.Color.BLACK, b.getBoard()[3][3].getColor());
+		/* end verification of state of board after playing e1W*/
+
+		assertEquals(Piece.Color.BLACK, b.getCurrentPlayer());
+
+		Piece p9 = new Piece("h8", Piece.Color.BLACK);
+		piecesToCapture = b.checkDiagonalUpLeft(p9);
+		assertEquals(3, piecesToCapture.size());
+		//check that other direction checks are empty
+		assertEquals(0, b.checkHorizontalRight(p9).size());
+		assertEquals(0, b.checkVerticalDown(p9).size());
+		assertEquals("e5W", piecesToCapture.get(0).toString());
+		assertEquals("f6W", piecesToCapture.get(1).toString());
+		assertEquals("g7W", piecesToCapture.get(2).toString());	
 	}
 }
